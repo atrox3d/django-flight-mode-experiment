250d3ec473e07612010edad5937e8f02ca98d30d
atrox3d <atrox3d@gmail.com>
2024-09-22 10:21:42 +0200

    setup modules

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+#  from .models import
+
+class TestUrls(TestCase):
+
+    def test_home(self):
+        response = self.client.get('/')
+        print(response)
