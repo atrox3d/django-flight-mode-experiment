e54b1a48109b211eb3c27c0fd68accec7ecf6ca5
atrox3d <atrox3d@gmail.com>
2024-09-22 10:25:42 +0200

    add helpers.testing

---
+++ myapp/helpers/testing.py
@@ -0,0 +1,16 @@
+BORDER_WIDTH = 30
+BORDER_CHAR = '/ '
+BORDER = BORDER_CHAR * BORDER_WIDTH
+
+# def bordered(fn):
+#     def wrapped(*args, **kwargs):
+#         print(BORDER)
+#         res =  fn(*args, **kwargs)
+#         print(BORDER)
+#         return res
+#     return wrapped
+
+def bordered(*args, border=BORDER, **kwargs):
+    print(BORDER)
+    print(*args, **kwargs)
+    print(BORDER)
--- myapp/tests.py
+++ myapp/tests.py
@@ -3,9 +3,10 @@ from django.test import TestCase
 # Create your tests here.
 
 #  from .models import
+from .helpers.testing import bordered
 
 class TestUrls(TestCase):
 
     def test_home(self):
         response = self.client.get('/')
-        print(response)
+        bordered(response)
