9e7d14e385d8b54ba358793bab6ef776133c6fdb
atrox3d <atrox3d@gmail.com>
2024-09-24 16:59:29 +0200

    define one to many relationship

--- myapp/models.py
+++ myapp/models.py
@@ -6,3 +6,16 @@ class MenuItems(models.Model):
     itemname = models.CharField(max_length=200)
     category = models.CharField(max_length=300)
     year = models.IntegerField()
+
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+
+class Menu(models.Model):
+    menu_item = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
+    category_id = models.ForeignKey(
+        to=MenuCategory,
+        on_delete=models.PROTECT,
+        default=None,
+        related_name='category_name'
+    )
