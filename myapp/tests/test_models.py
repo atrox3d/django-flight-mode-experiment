df065cb7532655956168944491434e113105b7cf
atrox3d <atrox3d@gmail.com>
2024-09-24 14:33:17 +0200

    add test for model

---
+++ myapp/tests/test_models.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+import json
+# Create your tests here.
+
+from ..models import MenuItems
+from ..helpers.testing import bordered, enable_output, disable_output
+
+
+class MenuItemsTest(TestCase):
+
+    def test_empty_menuitems(self):
+        self.assertEqual(MenuItems.objects.count(), 0)
+    
+    
