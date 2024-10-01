8d2c7d1a9a236b94374f10441dd04328467f67a8
atrox3d <atrox3d@gmail.com>
2024-10-01 11:55:03 +0200

    optimize imports

--- myapp/helpers/db/data.py
+++ myapp/helpers/db/data.py
@@ -1,28 +1,30 @@
 from ast import List
 import logging
 
-from myapp.helpers.db.management import load_json_menu
-from myapp.helpers.db.management import delete_initial_data
-from myapp.helpers.db.menu import get_categories_from_menu
-from myapp.helpers.db.menu import create_menu_categories
-from myapp.helpers.db.menu import create_menu_items
+# from .management import load_json_menu
+# from .management import delete_initial_data
+from . import management
+from .menu import get_categories_from_menu
+from .menu import create_menu_categories
+from .menu import create_menu_items
 
 from ... import models
 
 logger = logging.getLogger(__name__)
 
 def create_initial_data(
-        json_menu_path:str='menu.json', delete_existing=True
+        json_menu_path:str='menu.json', 
+        delete_existing=True
 ) -> None:
     logger.info(f'{json_menu_path=}, {delete_existing=}')
     if delete_existing:
-        delete_initial_data(
+        management.delete_initial_data(
             models.Menu, 
             models.MenuCategory, 
             models.MenuItems
         )
     
-    menu = load_json_menu(json_menu_path)
+    menu = management.load_json_menu(json_menu_path)
     categories = get_categories_from_menu(menu)
     menu_categories = create_menu_categories(*categories)
     menu_items = create_menu_items(menu)
