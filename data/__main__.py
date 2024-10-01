706d91535c7b9ac44c37d3bf5ba4610c77be229b
atrox3d <atrox3d@gmail.com>
2024-10-01 17:03:14 +0200

    add db reset and migrate

--- data/__main__.py
+++ data/__main__.py
@@ -2,6 +2,7 @@ import os
 import django
 import logging
 
+
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':  
@@ -15,8 +16,11 @@ if __name__ == '__main__':
     logger.info(f'setting up django')
     django.setup()
 
-    logger.info(f'importing myapp.helpers.db')
-    from data import data_creation
+    logger.info(f'importing data.data_creation')
+    from data import data_creation, dbtools
+
+    logger.info(f'resetting db')
+    dbtools.reset_db()
 
     logger.info(f'calling create_initial_data')
     data_creation.create_initial_data()
--- data/dbtools.py
+++ data/dbtools.py
@@ -1,7 +1,9 @@
 import json
 import logging
+from pathlib import Path
 
 from django.db.models import Model
+from django.core.management import call_command
 
 logger = logging.getLogger(__name__)
 
@@ -29,3 +31,11 @@ def delete_initial_data(
     for model in dbmodels:
         logger.info(f'deleting records from model {model.__qualname__}')
         model.objects.all().delete()
+
+def reset_db(db_path='db.sqlite3'):
+    if Path(db_path).exists():
+        logger.info(f'{db_path} exists, deleting')
+        Path(db_path).unlink()
+
+    logger.info(f'migrating myapp')
+    call_command('migrate', 'myapp')
