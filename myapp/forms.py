ac3431de73baff9ffba713e20b8d485e2b41ef4e
atrox3d <atrox3d@gmail.com>
2024-10-02 12:10:44 +0200

    add helptext

--- myapp/forms.py
+++ myapp/forms.py
@@ -10,7 +10,7 @@ class InputForm(forms.Form):
     first_name = forms.CharField(max_length=200)
     last_name = forms.CharField(max_length=200)
     shift = forms.ChoiceField(choices=SHIFTS)
-    time_log = forms.TimeField()
+    time_log = forms.TimeField(help_text='enter exact time')
 
 class ModelInputForm(forms.ModelForm):
     pass
