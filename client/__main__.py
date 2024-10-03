db06f2e3e02aa1708e43a304a3e025a73e8305e9
atrox3d <atrox3d@gmail.com>
2024-10-03 16:25:16 +0200

    refactor in functions

--- client/__main__.py
+++ client/__main__.py
@@ -1,3 +1,4 @@
+from turtle import pos
 import requests
 import logging
 
@@ -11,23 +12,31 @@ ENDPOINT = 'logger'
 URL = f'http://{SERVER}:{PORT}/{ENDPOINT}'
 logging.info(f'{URL = }')
 
-logging.info(f'getting csfrtoken...')
-csrftoken = requests.get(URL).cookies['csrftoken']
-logging.info(f'{csrftoken = }')
+def get_csrftoken(url:str):
+    logging.info(f'getting csfrtoken...')
+    csrftoken = requests.get(URL).cookies['csrftoken']
+    logging.info(f'{csrftoken = }')
+    return csrftoken
 
-header = {'X-CSRFToken': csrftoken}
-logging.info(f'setting {header = }')
+def post(url:str, csrftoken:str, data:dict) -> requests.Response:
+    header = {'X-CSRFToken': csrftoken}
+    logging.info(f'setting {header = }')
 
-cookies = {'csrftoken': csrftoken}
-logging.info(f'setting {cookies = }')
+    cookies = {'csrftoken': csrftoken}
+    logging.info(f'setting {cookies = }')
+
+    logging.info(f'setting {data = }')
+
+    logging.info('sending POST request')
+    response = requests.post(URL, data=data, headers=header, cookies=cookies)
+    logging.info(f'{response = }')
+
+    return response
 
 data={
     'first_name': 'first',
     'last_name': 'last',
     'time_log': '11:11',
 }
-logging.info(f'setting {data = }')
-
-logging.info('sending POST request')
-response = requests.post(URL, data=data, headers=header, cookies=cookies)
-logging.info(f'{response = }')
+crsftoken = get_csrftoken(URL)
+response = post(URL, crsftoken, data)
