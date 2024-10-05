fe8cca6ca14f7f2c9379b5a4f5015767747e8fc0
atrox3d <atrox3d@gmail.com>
2024-10-05 08:16:54 +0200

    add Reservation model

--- myapp/models/__init__.py
+++ myapp/models/__init__.py
@@ -6,3 +6,5 @@ from myapp.models.menucategory import MenuCategory
 from myapp.models.menu import Menu
 from myapp.models.customer import Customer
 from myapp.models.logger import Logger
+from myapp.models.reservation import Reservation
+
---
+++ myapp/models/reservation.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from myapp.models.dictmixin import DictMixin
+
+
+class Reservation(models.Model, DictMixin):
+    name = models.CharField(max_length=100, blank=True)
+    contact = models.CharField('Phone number', max_length=300)
+    time = models.TimeField()
+    count = models.IntegerField()
+    notes = models.CharField(max_length=300, blank=True)
